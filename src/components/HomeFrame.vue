518c2464d8da613b43382821b81744c1a8c7ee84
6bjs4112 <133857196+6bjs4112@users.noreply.github.com>
2023-11-21 15:51:29 +0900

    Update HomeFrame.vue
    폰트 수정

--- src/components/HomeFrame.vue
+++ src/components/HomeFrame.vue
@@ -75,7 +75,12 @@ export default {
 
 <style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;200;300;400;500;600;700;800;900&display=swap');
-//@import url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-POP.woff');
+@font-face {
+    font-family: 'ONE-Mobile-POP';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-POP.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
 .leftProfile{
   width: 20%;
   position: absolute; top: 10%; left: 5%;
